1094a463bf48fbc6fd0458d375c7d9c223c60851
Hasan Ali <122424731+Hassanali477@users.noreply.github.com>
2024-05-24 16:43:51 +0500

    1:App Icon 2:Splash Screen 3:Login/SignUp 4:Dashboard Screen 5:Profile Screen + Drawer These things are done without API Integration

--- index.js
+++ index.js
@@ -5,5 +5,17 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider} from 'react-redux';
+import configureStore from './src/redux/store/configureStore';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+const RNRedux = () => {
+  <Provider store={store}>
+    <App />
+  </Provider>;
+};
+
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+AppRegistry.registerComponent(appName, () => RNRedux);
